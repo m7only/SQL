e4815ba12f151fd1dd81646cc5950e07e9c551f1
Only <ongranly@ngs.ru>
2023-03-08 09:04:30 +0700

    ДЗ Выборка данных и вложенные запросы

--- src/query.sql
+++ src/query.sql
@@ -1,22 +1,21 @@
 -- Задание 1
-INSERT INTO employee(first_name, last_name, gender, age) VALUES( 'Вася', 'Васий', 'муж', 159);
-INSERT INTO employee(first_name, last_name, gender, age) VALUES('Мария', 'Магдалина', 'жен', 159);
-INSERT INTO employee(first_name, last_name, gender, age) VALUES('Жанна', 'д''Арк', 'жен',19);
-INSERT INTO employee(first_name, last_name, gender, age) VALUES('Лаврентий', 'Берия', 'муж', 54);
-INSERT INTO employee(first_name, last_name, gender, age) VALUES('Владимир', 'Ульянов', 'муж',53);
-SELECT first_name AS Имя, last_name AS Фамилия FROM employee;
-SELECT * FROM employee WHERE age < 30 OR age > 50;
-SELECT * FROM employee WHERE age BETWEEN 30 AND 50;
-SELECT * FROM employee ORDER BY last_name DESC;
-SELECT * FROM employee WHERE first_name LIKE '____%';
-SELECT * FROM employee WHERE LENGTH(first_name) > 4;
+INSERT INTO employee(first_name, last_name, gender, age) VALUES( 'Вася', 'Васий', 'муж', 159); -- 1
+INSERT INTO employee(first_name, last_name, gender, age) VALUES('Мария', 'Магдалина', 'жен', 159); -- 1
+INSERT INTO employee(first_name, last_name, gender, age) VALUES('Жанна', 'д''Арк', 'жен',19); -- 1
+INSERT INTO employee(first_name, last_name, gender, age) VALUES('Лаврентий', 'Берия', 'муж', 54); -- 1
+INSERT INTO employee(first_name, last_name, gender, age) VALUES('Владимир', 'Ульянов', 'муж',53); -- 1
+SELECT first_name AS Имя, last_name AS Фамилия FROM employee; -- 2
+SELECT * FROM employee WHERE age < 30 OR age > 50; -- 3
+SELECT * FROM employee WHERE age BETWEEN 30 AND 50; -- 4
+SELECT * FROM employee ORDER BY last_name DESC; -- 5
+SELECT * FROM employee WHERE first_name LIKE '____%'; -- 6
+SELECT * FROM employee WHERE LENGTH(first_name) > 4; -- 6
 
 -- Задание 2
-UPDATE employee SET first_name='Павел' WHERE id = 3;
-UPDATE employee SET first_name='Владимир' WHERE id = 7;
-SELECT first_name AS Имя, SUM(age) as Суммарный_возраст FROM employee GROUP BY first_name;
+UPDATE employee SET first_name='Павел' WHERE id = 3; -- 1
+UPDATE employee SET first_name='Владимир' WHERE id = 7; -- 1
+SELECT first_name AS Имя, SUM(age) as Суммарный_возраст FROM employee GROUP BY first_name; -- 2
 -- Выведите имя и самый юный возраст, соответствующий имени. Не совсем понял, самый минимальный среди всех или миимальный для каждого имени, сделал оба варианта
-SELECT first_name AS Имя, age AS Минимальный_возраст FROM employee WHERE age = (SELECT MIN(age) FROM employee);
-SELECT first_name AS Имя, MIN(age) AS Минимальный_возраст FROM employee GROUP BY Имя;
-
-SELECT first_name, MAX(age) AS max_age FROM employee GROUP BY first_name HAVING COUNT(first_name) > 1 ORDER BY max_age ASC;
+SELECT first_name AS Имя, age AS Минимальный_возраст FROM employee WHERE age = (SELECT MIN(age) FROM employee); -- 3
+SELECT first_name AS Имя, MIN(age) AS Минимальный_возраст FROM employee GROUP BY Имя; -- 3
+SELECT first_name, MAX(age) AS max_age FROM employee GROUP BY first_name HAVING COUNT(first_name) > 1 ORDER BY max_age ASC; -- 4
